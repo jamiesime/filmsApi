df3e750709af65c233153dc9740c97589d3b90c8
Jamie Sime <jamiesime@gmail.com>
2017-12-12 10:02:42 +0000

    added restful routes for film api

--- controllers/films.js
+++ controllers/films.js
@@ -2,3 +2,33 @@
 var films = require('../client/src/models/films')();
 var Film = require('../client/src/models/film');
 var Review = require('../client/src/models/review');
+
+var express = require("express");
+var filmsRouter = new express.Router();
+
+filmsRouter.get("/", function(req, res){
+  res.json(films);
+});
+
+filmsRouter.get("/:id", function(req, res){
+  res.json(films[req.params.id]);
+});
+
+filmsRouter.post("/", function(req, res){
+  var newFilm = new Film(req.body.film)
+  films.push(newFilm);
+  res.json(films);
+});
+
+filmsRouter.put("/:id", function(req, res){
+  var newFilm = new Film(req.body.film);
+  films[req.params.id] = newFilm;
+  res.json(films);
+});
+
+filmsRouter.delete("/:id", function(req, res){
+  films.splice(req.params.id, 1);
+  res.json(films);
+});
+
+module.exports = filmsRouter;
